aad03e05256622ec4a2d0e58f67a3e5318a7638b
Justin M <jdm216@gmail.com>
2021-06-26 17:54:47 -0500

    completes coding exercise 5 decimalComparator

--- src/com/Justin/DecimalComparator.java
+++ src/com/Justin/DecimalComparator.java
@@ -1,4 +1,9 @@
 package com.Justin;
 
 public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces(double x, double y) {
+        return (int) (x * 1000) == (int) (y * 1000);
+
+
+    }
 }
